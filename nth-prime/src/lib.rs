32285b35598504769ccf0af4f5550206f0b465e2
Will Fuger <wfuger@gmail.com>
2018-05-24 20:58:08 -0500

    solution

--- nth-prime/src/lib.rs
+++ nth-prime/src/lib.rs
@@ -1,3 +1,22 @@
 pub fn nth(n: u32) -> Option<u32> {
-    unimplemented!("What is the {}th prime number?", n)
+    let mut prime: Option<u32> = None;
+    let mut current: u32 = 2;
+    let mut count: u32 = 0;
+    while count < n {
+        if is_prime(current) {
+            prime = Some(current);
+            count += 1;
+        }
+        current += 1;
+    }
+    return prime
+}
+
+fn is_prime(num: u32) -> bool {
+    for i in 2..num {
+        if num % i == 0 {
+            return false;
+        }
+    }
+    true
 }
