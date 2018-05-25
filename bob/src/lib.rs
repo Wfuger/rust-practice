014d71b55e7d09977f04519f0da451ecb37c3cb9
Will Fuger <wfuger@gmail.com>
2018-05-25 07:14:45 -0500

    row status

--- bob/src/lib.rs
+++ bob/src/lib.rs
@@ -1,3 +1,14 @@
 pub fn reply(message: &str) -> &str {
-    unimplemented!("have Bob reply to the incoming message: {}", message)
+    let mut reply = "Whatever.";
+    let yell = message == message.to_uppercase();
+    let question = message.contains("?");
+    let yell_question = yell && question;
+    if yell_question {
+        reply = "Calm down, I know what I'm doing!";
+    } else if yell {
+        reply = "Whoa, chill out!"
+    } else if question {
+        reply = "Sure."
+    }
+    return reply;
 }
